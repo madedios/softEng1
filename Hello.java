11b7b683804f7e4856dd79ff51d9f94c3130d564
Marco Angelo De Dios <madedios.it@gmail.com>
2023-08-30 10:32:50 +0800

    modified Hello.java and added MyNewFile.txt

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main( String[] args ){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is DE DIOS, Marco Angelo!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
